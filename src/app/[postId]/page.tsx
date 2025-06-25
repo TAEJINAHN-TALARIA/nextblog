c139bdaf13e0b921b0da9feba51d359b058d06fb
안태진 <tahn41@gmail.com>
2025-06-25 22:50:28 +0900

    20250625

--- src/app/[postId]/page.tsx
+++ src/app/[postId]/page.tsx
@@ -2,20 +2,20 @@ import { createClient } from "../utils/supabase/server";
 import { notFound } from "next/navigation";
 import PostViewClient from "./PostViewClient";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: {
-    postId: string;
-  };
-}) {
+type PostPageProps = {
+  params: Promise<{ postId: string }>; // params를 Promise로 정의
+};
+
+export async function generateMetadata({ params }: PostPageProps) {
   // supabase client를 불러올 때는 반드시 async - await를 사용하자!
   const sbClient = await createClient();
+  // params를 불러오는 것도 await 처리
+  const PostPageParams = await params;
   // supabase에서 데이터를 불러올 때도 반드시 async - await를 사용하자!
   const { data: post, error } = await sbClient
     .from("post")
     .select("title, summary")
-    .eq("postId", params.postId)
+    .eq("postId", PostPageParams.postId)
     .single();
   // error가 발생했거나 데이터가 존재하지 않으면, metadata를 아래와 같이 설정함!
   if (error || !post) {
@@ -31,20 +31,16 @@ export async function generateMetadata({
   };
 }
 
-export default async function PostPage({
-  params,
-}: {
-  params: {
-    postId: string;
-  };
-}) {
+export default async function PostPage({ params }: PostPageProps) {
   // supabase client를 불러올 때는 반드시 async - await를 사용하자!
   const sbClient = await createClient();
+  // params를 불러오는 것도 await 처리
+  const PostPageParams = await params;
   // supabase에서 데이터를 불러올 때도 반드시 async - await를 사용하자!
   const { data: post, error } = await sbClient
     .from("post")
     .select("*")
-    .eq("postId", params.postId)
+    .eq("postId", PostPageParams.postId)
     .single();
   // 포스트 데이터를 불러올 때 에러가 발생하거나 데이터가 없으면 Not Found 화면을 띄움!
   if (error || !post) {
