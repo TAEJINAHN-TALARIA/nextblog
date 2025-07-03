a8338eb2509623d672f73566b6436c177d12b08b
안태진 <tahn41@gmail.com>
2025-07-03 22:28:24 +0900

    20250703

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/editor/",
+      disallow: "/editor",
     },
     sitemap: "https://talariablog.vercel.app/sitemap.xml",
   };
